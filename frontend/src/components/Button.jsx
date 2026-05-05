d70f8627318019f00d8babeebc05787f5f996fae
Saqlain <mdsaqlain2486@gmail.com>
2026-05-05 08:03:28 +0530

    Adde funtionality to the buttons

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './Button.css';
 
-const Button = ({ children, variant = 'primary', icon, onClick, className = '' }) => {
+const Button = ({ children, variant = 'primary', icon, onClick, className = '', ...props }) => {
   return (
-    <button className={`btn btn-${variant} ${className}`} onClick={onClick}>
+    <button className={`btn btn-${variant} ${className}`} onClick={onClick} {...props}>
       {icon && <span className="btn-icon">{icon}</span>}
       {children}
     </button>
